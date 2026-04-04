80573fc12f1b49678c6caa2534c19f5568f05c49
Luciano Zangeronimo <zangeronimo@gmail.com>
2026-04-04 18:31:03 -0300

    shore: create culinary category entity

---
+++ WEBEditorAPI.Domain/Entities/Culinary/Category.cs
@@ -0,0 +1,30 @@
+using WEBEditorAPI.Domain.Enums;
+using WEBEditorAPI.Domain.ValueObjects;
+
+namespace WEBEditorAPI.Domain.Entities.Culinary;
+
+public class Category : Entity
+{
+    public Slug Slug { get; private set; } = null!;
+    public string Name { get; private set; } = null!;
+    public Guid CompanyId { get; private set; }
+    public Status Active { get; private set; }
+
+    public Category(Slug slug, string name, Status active, Guid companyId) : base()
+    {
+        Slug = slug;
+        Name = name;
+        Active = active;
+        CompanyId = companyId;
+    }
+
+    protected Category() : base() { }
+
+    public void Update(Slug newSlug, string newName, Status newActive)
+    {
+        Slug = newSlug;
+        Name = newName;
+        Active = newActive;
+        Touch();
+    }
+}
---
+++ WEBEditorAPI.Domain/Enums/Status.cs
@@ -0,0 +1,7 @@
+namespace WEBEditorAPI.Domain.Enums;
+
+public enum Status : byte
+{
+    Inactive = 0,
+    Active = 1
+}
