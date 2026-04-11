115616a9463b1f69376279bd24b6ff7130d6ebf3
Luciano Zangeronimo <zangeronimo@gmail.com>
2026-04-11 07:05:39 -0300

    refactor: add Category to Level aggregate root

--- WEBEditorAPI.Infrastructure/DI/CulinaryModuleDI.cs
+++ WEBEditorAPI.Infrastructure/DI/CulinaryModuleDI.cs
@@ -3,13 +3,10 @@ using WEBEditorAPI.Application.DTOs;
 using WEBEditorAPI.Application.DTOs.Culinary;
 using WEBEditorAPI.Application.Interfaces;
 using WEBEditorAPI.Application.Requests.UseCases;
-using WEBEditorAPI.Application.Requests.UseCases.Culinary.Categories;
 using WEBEditorAPI.Application.Requests.UseCases.Culinary.Levels;
-using WEBEditorAPI.Application.UseCases.Culinary.Categories;
 using WEBEditorAPI.Application.UseCases.Culinary.Levels;
 using WEBEditorAPI.Domain.Interfaces.Repository.Culinary;
 using WEBEditorAPI.Infrastructure.Repositories.Culinary;
-using WEBEditorAPI.Infrastructure.Repositories.System;
 
 namespace WEBEditorAPI.Infrastructure.DI;
 
@@ -23,11 +20,7 @@ public static class CulinaryModuleDI
         services.AddScoped<IUseCase<CreateLevelRequest, LevelDto>, CreateLevelUC>();
         services.AddScoped<IUseCase<UpdateLevelRequest, LevelDto>, UpdateLevelUC>();
 
-        services.AddScoped<IUseCase<GetAllCategoriesFilterRequest, PaginationResult<CategoryDto>>, GetAllCategoryUC>();
-        services.AddScoped<IUseCase<GetByIdRequest, CategoryDto>, GetCategoryByIdUC>();
-
         // Repositories
-        services.AddScoped<ICategoryRepository, CategoryRepository>();
         services.AddScoped<ILevelRepository, LevelRepository>();
 
         return services;
