8e152e74667cc26744685998ea0f6e42c92ddf29
Luciano Zangeronimo <zangeronimo@gmail.com>
2026-06-09 18:45:36 -0300

    fix: fix allowedFields Status

--- Nexora.Infrastructure/Repositories/Culinary/CategoryRepository.cs
+++ Nexora.Infrastructure/Repositories/Culinary/CategoryRepository.cs
@@ -49,7 +49,7 @@ public class CategoryRepository(CulinaryDbContext context) : ICategoryRepository
             [
                 "Name",
                 "Slug",
-                "Active"
+                "Status"
             ]
         );
 
