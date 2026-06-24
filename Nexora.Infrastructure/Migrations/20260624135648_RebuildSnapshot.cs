aecc38150520f4e43ceba4903986dd0f823d99ba
Luciano Zangeronimo <zangeronimo@gmail.com>
2026-06-24 10:58:39 -0300

    chore: fix migrations

---
+++ Nexora.Infrastructure/Migrations/20260624135648_RebuildSnapshot.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nexora.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RebuildSnapshot : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "client_id",
+                table: "core_api_clients",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "client_id",
+                table: "core_api_clients",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
