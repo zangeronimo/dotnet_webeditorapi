b97f7e2c7c6bc8145b9dc04d2f2d348ccb3b2a19
Luciano Zangeronimo <zangeronimo@gmail.com>
2026-06-25 10:56:18 -0300

    refactor: order by status desc

--- Nexora.Api/Models/Culinary/Recipes/GetAllRecipesFilterModel.cs
+++ Nexora.Api/Models/Culinary/Recipes/GetAllRecipesFilterModel.cs
@@ -4,7 +4,7 @@ namespace Nexora.Api.Models.Culinary.Recipes;
 
 public class GetAllRecipesFilterModel : PaginationModel
 {
-    public string OrderBy { get; init; } = "Id";
+    public string OrderBy { get; init; } = "Status";
     public bool Desc { get; init; } = false;
     public string? Name { get; init; }
     public Status? Status { get; init; }
